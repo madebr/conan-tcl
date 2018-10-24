2de659c30864a894cc6d3e9782d99a2c0f4bf723
Anonymous Maarten <anonymous.maarten@gmail.com>
2018-10-24 22:31:17 +0200

    Add files

---
+++ conanfile.py
@@ -0,0 +1,158 @@
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
+from conans.errors import ConanExceptionInUserConanfileMethod
+import os
+
+
+class TclConan(ConanFile):
+    name = 'tcl'
+    version = '8.6.8'
+    license = 'TCL'
+    url = 'https://github.com/madebr/conan-tcl'
+    description = 'Tcl is a very powerful but easy to learn dynamic programming language.'
+    settings = 'os', 'compiler', 'build_type', 'arch'
+    options = {
+        'fPIC': [True, False],
+        'shared': [True, False]
+    }
+    default_options = {
+        'fPIC': False,
+        'shared': False,
+    }
+    _source_subfolder = 'sources'
+
+    @property
+    def _is_mingw_windows(self):
+        return self.settings.os == 'Windows' and self.settings.compiler == 'gcc'
+
+    def configure(self):
+        if self.settings.compiler != 'Visual Studio':
+            del self.settings.compiler.libcxx
+
+    def build_requirements(self):
+        if self._is_mingw_windows:
+            self.build_requires('msys2_installer/latest@bincrafters/stable')
+
+    def requirements(self):
+        self.requires('zlib/1.2.11@conan/stable')
+
+    def source(self):
+        url = 'https://prdownloads.sourceforge.net/tcl/tcl{}-src.tar.gz'.format(self.version)
+        tools.get(url)
+        extracted_dir = '{}{}'.format(self.name, self.version)
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def config_options(self):
+        if self.settings.os == 'Windows':
+            del self.options.fPIC
+        else:
+            if self.options.shared:
+                self.options.fPIC = True
+                # del self.options.fPIC  # Does not make sense.
+
+    @property
+    def _configure_dir(self):
+        if self.settings.os == 'Windows':
+            return os.path.join(self.source_folder, self._source_subfolder, 'win')
+        elif self.settings.os == 'Linux':
+            return os.path.join(self.source_folder, self._source_subfolder, 'unix')
+        elif self.settings.os == 'Macos':
+            return os.path.join(self.source_folder, self._source_subfolder, 'macosx')
+        else:
+            raise ConanExceptionInUserConanfileMethod('Invalid os: {}'.format(self.settings.os))
+
+    def _get_auto_tools(self):
+        autoTools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+        if self._is_mingw_windows:
+            # FIXME: bug in zlib/1.2.11@conan/stable ??
+            autoTools.libs.append('z')
+        return autoTools
+
+    def _build_nmake(self, target='release'):
+        opts = []
+        # https://core.tcl.tk/tips/doc/trunk/tip/477.md
+        if not self.options.shared:
+            opts.append('static')
+        if self.settings.build_type == 'Debug':
+            opts.append('symbols')
+        if 'MD' in self.settings.compiler.runtime:
+            opts.append('msvcrt')
+        else:
+            opts.append('nomsvcrt')
+        vcvars_command = tools.vcvars_command(self.settings)
+        self.run(
+            '{vcvars} && nmake -nologo -f "{cfgdir}/makefile.vc" shell INSTALLDIR="{pkgdir}" OPTS={opts} {target}'.format(
+                vcvars=vcvars_command,
+                cfgdir=self._configure_dir,
+                pkgdir=self.package_folder,
+                opts=','.join(opts),
+                target=target,
+            ), cwd=self._configure_dir,
+        )
+
+    def _build_autotools(self):
+        conf_args = [
+            '--enable-threads',
+            '--enable-shared' if self.options.shared else '--disable-shared',
+            '--enable-symbols' if self.settings.build_type == 'Debug' else '--disable-symbols',
+        ]
+        if self.settings.arch == 'x86_64':
+            conf_args.append('--enable-64bit')
+        autoTools = self._get_auto_tools()
+        autoTools.configure(configure_dir=self._configure_dir, args=conf_args)
+
+        # https://core.tcl.tk/tcl/tktview/840660e5a1
+        for root, _, files in os.walk(self.build_folder):
+            if 'Makefile' in files:
+                tools.replace_in_file(os.path.join(root, 'Makefile'), '-Dstrtod=fixstrtod', '', strict=False)
+
+        with tools.chdir(self.build_folder):
+            autoTools.make()
+
+    def build(self):
+        if self.settings.compiler == 'Visual Studio':
+            self._build_nmake()
+        else:
+            self._build_autotools()
+
+    def package(self):
+        if self.settings.compiler == 'Visual Studio':
+            self._build_nmake('install')
+        else:
+            with tools.chdir(self.build_folder):
+                autoTools = self._get_auto_tools()
+                autoTools.install()
+        self.copy(pattern='license.terms', dst='licenses', src=self._source_subfolder)
+
+    def package_info(self):
+        libs = []
+        libdirs = []
+        for root, _, _ in os.walk(os.path.join(self.package_folder, 'lib'), topdown=False):
+            newlibs = tools.collect_libs(self, root)
+            if newlibs:
+                libs.extend(newlibs)
+                libdirs.append(root)
+        if self._is_mingw_windows:
+            # FIXME: bug in zlib/1.2.11@conan/stable ??
+            libs.append('z')
+        if self.settings.compiler == 'Visual Studio':
+            libs.extend(['netapi32'])
+        else:
+            libs.extend(['m', 'pthread'])
+            if self._is_mingw_windows:
+                libs.extend(['ws2_32', 'netapi32', 'userenv'])
+            else:
+                libs.append('dl')
+        defines = []
+        if not self.options.shared:
+            defines.append('STATIC_BUILD')
+        self.cpp_info.defines = defines
+        self.cpp_info.bindirs = ['bin']
+        self.cpp_info.libdirs = libdirs
+        self.cpp_info.libs = libs
+        self.cpp_info.includedirs = ['include']
+        self.env_info.TCL_LIBRARY = os.path.join(self.package_folder, 'lib', 'tcl{}'.format('.'.join(self.version.split('.')[:2])))
+        if self.settings.os == 'Macos':
+            self.cpp_info.exelinkflags.append('-framework Cocoa')
+            self.cpp_info.sharedlinkflags = self.cpp_info.exelinkflags
---
+++ test_package/conanfile.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from conans import ConanFile, CMake, tools, RunEnvironment
+
+
+class TclTestConan(ConanFile):
+    settings = 'os', 'compiler', 'build_type', 'arch'
+    generators = 'cmake'
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def imports(self):
+        self.copy('*.dll', dst='bin', src='bin')
+        self.copy('*.dylib*', dst='bin', src='lib')
+        self.copy('*.so*', dst='bin', src='lib')
+
+    def test(self):
+        with tools.environment_append(RunEnvironment(self).vars):
+            bin_path = os.path.join('bin', 'embed_tcl')
+            if self.settings.os == 'Macos':
+                self.run('DYLD_LIBRARY_PATH={} {}'.format(os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
+            else:
+                self.run(bin_path)
